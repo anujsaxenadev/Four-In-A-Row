fe9edb973a615dc3be599fe033c82eb8e0cfd9fc
Anuj <beaver@Beaver>
2019-02-09 15:29:07 +0530

    initializing Game

--- js/Game.js
+++ js/Game.js
@@ -1,4 +1,17 @@
 // Class for Game
 class Game{
+    constructor(){
+        this.board = new Board();
+        this.player = this.createPlayers();
+        this.ready = false;
+    }
 
+    /** 
+     * Creates two player objects
+     * @return  {Array}    An array of two Player objects.
+     */
+    createPlayers(){
+        const players = [new Player("Player 1", 1, "#e15258", true), new Player("Player 2", 2, "#e59a13")];
+        return players;
+    }
 }
