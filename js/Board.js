98a4bcae977a32489b5e096d725a8835f9b36f4c
Anuj <beaver@Beaver>
2019-02-09 15:09:03 +0530

    Creating Spaces

--- js/Board.js
+++ js/Board.js
@@ -1,4 +1,27 @@
 // Class for Game Board
 class Board{
+    constructor(){
+        this.rows = 6;
+        this.columns = 7;
+        this.spaces = this.createSpaces();
+    }
 
+    /** 
+     * Generates 2D array of spaces. 
+     * @return  {Array}     An array of space objects
+     */
+    createSpaces(){
+        const spaces = [];
+
+        for(let i = 0; i < this.columns; i++){
+            const column = [];
+
+            for(let j = 0; j < this.rows; j++){
+                let space = new Space(j,i);
+                column.push(space);
+            }
+            spaces.push(column);
+        }
+        return spaces;
+    }
 }
