f8a8374768b1e5417dfd015e99e9ca0c824a086d
Anuj <beaver@Beaver>
2019-02-09 16:38:56 +0530

    initializing game on click of start button

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Creating the Game
+const game = new Game();
+
+const beginGame = document.querySelector('#begin-game');
+beginGame.addEventListener('click', function(){
+    game.startGame;
+    this.style.display = 'none';
+    document.querySelector('#play-area').style.opacity = '1';
+});
