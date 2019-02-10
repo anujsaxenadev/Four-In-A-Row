0472ab4db4ac4fda9721e2ca510a9a52ae233dcc
Anuj <beaver@Beaver>
2019-02-10 23:17:49 +0530

    drawing html for token

--- js/Token.js
+++ js/Token.js
@@ -5,4 +5,23 @@ class Token{
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
     }
+
+    /**
+     * Creating HTML for Token.
+     */
+    drawHTMLToken(){
+        const tokenDiv = document.createElement('div');
+        document.querySelector('#game-board-underlay').appendChild(tokenDiv);
+        tokenDiv.setAttribute('id', this.id);
+        tokenDiv.setAttribute('class', 'token');
+        tokenDiv.style.backgroundColor = this.owner.color;
+    }
+
+    /**
+     * getter method for HTML Token.
+     * @return {element} Getting html for the token.
+     */
+    get HTMLToken(){
+        return this.drawHTMLToken();
+    }
 }
