9e4076fa3e68b626070b38a759fa81b358a98ea2
Anuj <beaver@Beaver>
2019-02-10 22:26:39 +0530

    bug fix - calling function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const game = new Game();
 
 const beginGame = document.querySelector('#begin-game');
 beginGame.addEventListener('click', function(){
-    game.startGame;
+    game.startGame();
     this.style.display = 'none';
     document.querySelector('#play-area').style.opacity = '1';
 });
