e28e5e197db7c228d443fbbb8a06afa4b1c172f1
Anuj <beaver@Beaver>
2019-02-11 16:07:45 +0530

    mark and owner methods

--- js/Space.js
+++ js/Space.js
@@ -21,7 +21,25 @@ class Space{
         svgSpace.setAttributeNS(null, "r", this.radius - 8);
         svgSpace.setAttributeNS(null, "fill", "black");
         svgSpace.setAttributeNS(null, "stroke", "none");
-        
+
         document.getElementById("mask").appendChild(svgSpace);
+    }
+
+    /**
+     * Method for marking the space with the dropped token.
+     */
+    mark(token){
+        this.token = token;
+    }
+
+    /**
+     * Getter method for getting the owner of the token that was dropped into this space.
+     */
+    get owner(){
+        if(this.token === null){
+            return null;
+        }
+        else
+            return this.token.owner;
     }
 }
